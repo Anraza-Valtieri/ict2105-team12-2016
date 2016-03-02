109745bfc093b5887d4502999676724a1d36cc23
lotus <kyroguex@gmail.com>
2016-03-03 01:35:04 +0800

    Demo recyclerview

---
+++ app/src/main/java/com/example/anton/test_application/Business/QueueInfo.java
@@ -0,0 +1,16 @@
+package com.example.anton.test_application.Business;
+
+/**
+ * Created by L on 3/2/2016.
+ */
+public class QueueInfo {
+     int noOfCustomers;
+     String title;
+
+    public QueueInfo(String title,int numberOfCusts){
+        this.noOfCustomers = numberOfCusts;
+        this.title = title;
+    }
+
+
+}
--- app/src/main/java/com/example/anton/test_application/MainActivity.java
+++ app/src/main/java/com/example/anton/test_application/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.anton.test_application;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.anton.test_application.Business.QueueDashboardActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,QueueDashboardActivity.class);
+                startActivity(intent);
             }
         });
     }
