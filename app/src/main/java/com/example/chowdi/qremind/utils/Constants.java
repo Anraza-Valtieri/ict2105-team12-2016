d2244d5b45f34206138474c6b0d8423bd8e54633
lotus <kyroguex@gmail.com>
2016-03-06 02:23:25 +0800

    Add utils folder and Constants class

---
+++ app/src/main/java/com/example/chowdi/qremind/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.example.chowdi.qremind.utils;
+
+/**
+ * Created by L on 3/6/2016.
+ */
+public final class Constants {
+
+    //prevent instantiation
+    private Constants(){
+
+    }
+
+    public static final String  FIREBASE_MAIN = "https://qremind1.firebaseio.com";
+    public static final String  FIREBASE_CUSTOMER = "https://qremind1.firebaseio.com/AppUserAccount/Customer";
+    public static final String  FIREBASE_VENDOR = "https://qremind1.firebaseio.com/AppUserAccount/Vendor";
+
+}
