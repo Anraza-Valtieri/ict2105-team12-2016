31e327a8775702805c1ef2906034427fb369a562
antonsalim <anton_salim@hotmail.com>
2016-03-09 17:13:53 +0800

    Added some new contanst string variables

--- app/src/main/java/com/example/chowdi/qremind/utils/Constants.java
+++ app/src/main/java/com/example/chowdi/qremind/utils/Constants.java
@@ -10,8 +10,18 @@ public final class Constants {
 
     }
 
+    // For Firebase
     public static final String  FIREBASE_MAIN = "https://qremind1.firebaseio.com";
     public static final String  FIREBASE_CUSTOMER = "https://qremind1.firebaseio.com/AppUserAccount/Customer";
     public static final String  FIREBASE_VENDOR = "https://qremind1.firebaseio.com/AppUserAccount/Vendor";
 
+    // For shared preferences
+    public static final String SHARE_PREF_LINK = "QREMIND_SP";
+    public static final String SHAREPREF_EMAIL = "EMAIL";
+    public static final String SHAREPREF_PHONE_NO = "PHONE_NO";
+    public static final String SHAREPREF_ROLE = "ROLE";
+
+    // For others
+    public static final String ROLE_VENDOR = "VENDOR";
+    public static final String ROLE_CUSTOMER = "CUSTOMER";
 }
