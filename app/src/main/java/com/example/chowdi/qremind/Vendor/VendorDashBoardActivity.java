4c2d4f3430b4255c4cc6d4401a75e47894729432
lotus <kyroguex@gmail.com>
2016-03-13 18:12:08 +0800

    Initial Commit of dashboard

---
+++ app/src/main/java/com/example/chowdi/qremind/Vendor/VendorDashBoardActivity.java
@@ -0,0 +1,15 @@
+package com.example.chowdi.qremind.Vendor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.chowdi.qremind.R;
+
+public class VendorDashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_dash_board);
+    }
+}
