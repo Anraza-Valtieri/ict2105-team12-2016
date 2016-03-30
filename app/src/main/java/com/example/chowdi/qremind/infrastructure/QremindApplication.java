4d0555d7dd923d0c73b83732718a163317dabe17
antonsalim <anton_salim@hotmail.com>
2016-03-30 20:03:48 +0800

    nothing change

--- app/src/main/java/com/example/chowdi/qremind/infrastructure/QremindApplication.java
+++ app/src/main/java/com/example/chowdi/qremind/infrastructure/QremindApplication.java
@@ -2,7 +2,11 @@ package com.example.chowdi.qremind.infrastructure;
 
 import android.app.Application;
 
+import com.example.chowdi.qremind.utils.Constants;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 /**
  * Created by L on 3/26/2016.
@@ -14,11 +18,14 @@ public class QremindApplication extends Application {
 
     private Customer customerUser;
     private Vendor vendorUser;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        //Initialize Firebase library in Android context before any Firebase reference is created or used
+        //Initialize Firebase library in Android context before any FireBase reference is created or used
         Firebase.setAndroidContext(getApplicationContext());
+        //TODO: change avatarImage to avatarURL from user
+
     }
 
     public Customer getCustomerUser() {
