3ca04d4df380ef4e8510aad7f56b6d1bf36922ac
antonsalim <anton_salim@hotmail.com>
2016-03-31 07:03:27 +0800

    Updated the infrastructure

--- app/src/main/java/com/example/chowdi/qremind/infrastructure/Vendor.java
+++ app/src/main/java/com/example/chowdi/qremind/infrastructure/Vendor.java
@@ -1,6 +1,10 @@
 package com.example.chowdi.qremind.infrastructure;
 
+import android.graphics.Bitmap;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Map;
 
 /**
  * Created by L on 3/5/2016.
@@ -11,6 +15,9 @@ public class Vendor {
     private String firstname;
     private String lastname;
     private String phoneno;
+    private String image;
+    private Bitmap myImage;
+    private Map<String, Object> shops;
 
     public Vendor(){
 
@@ -27,15 +34,55 @@ public class Vendor {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getFirstname() {
         return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
     }
 
     public String getLastname() {
         return lastname;
     }
 
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
     public String getPhoneno() {
         return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Bitmap getMyImage() {
+        return myImage;
+    }
+
+    public void setMyImage(Bitmap myImage) {
+        this.myImage = myImage;
+    }
+
+    public Map<String, Object> getShops() {
+        return shops;
+    }
+
+    public void setShops(Map<String, Object> shops) {
+        this.shops = shops;
     }
 }
