806612ad252677619bc95f5f7436d1a8992c22da
antonsalim <anton_salim@hotmail.com>
2016-03-30 20:49:56 +0800

    updated code

--- app/src/main/java/com/example/chowdi/qremind/LogoCoverPageActivity.java
+++ app/src/main/java/com/example/chowdi/qremind/LogoCoverPageActivity.java
@@ -1,6 +1,7 @@
 package com.example.chowdi.qremind;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,7 +12,18 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class LogoCoverPageActivity extends AppCompatActivity{
+import com.example.chowdi.qremind.Customer.CustomerHomePageActivity;
+import com.example.chowdi.qremind.activities.BaseActivity;
+import com.example.chowdi.qremind.infrastructure.Customer;
+import com.example.chowdi.qremind.infrastructure.Vendor;
+import com.example.chowdi.qremind.utils.Commons;
+import com.example.chowdi.qremind.utils.Constants;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class LogoCoverPageActivity extends BaseActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -20,9 +32,62 @@ public class LogoCoverPageActivity extends AppCompatActivity{
         // Sleep for 2 seconds
         SystemClock.sleep(2000);
 
-        Intent intent = new Intent(LogoCoverPageActivity.this, Login_RegisterActivity.class);
-        startActivity(intent);
-        finish();
+
+        // Check if there is already an authorisation for firebase which the user application have logged in previously
+        Firebase fbRef = new Firebase(Constants.FIREBASE_MAIN);
+        SharedPreferences prefs;
+        // if there is valid authorisation, redirect to next activity
+        if(fbRef.getAuth() != null)
+        {
+            prefs = getSharedPreferences(Constants.SHARE_PREF_LINK, MODE_PRIVATE);
+            String role = prefs.getString(Constants.SHAREPREF_ROLE, null);
+            String phoneNo = prefs.getString(Constants.SHAREPREF_PHONE_NO, null);
+            retrieveAccountInfo(phoneNo, role);
+        }
+        else {
+            nextActivityAfterLogin(Login_RegisterActivity.class);
+        }
     }
 
+    private void retrieveAccountInfo(String phoneNo, final String role)
+    {
+        Firebase accFbRef;
+        if(role.equals(Constants.ROLE_CUSTOMER))
+            accFbRef= new Firebase(Constants.FIREBASE_CUSTOMER+"/"+phoneNo);
+        else
+            accFbRef= new Firebase(Constants.FIREBASE_VENDOR+"/"+phoneNo);
+        accFbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(role.equals(Constants.ROLE_CUSTOMER))
+                {
+                    Customer custUser = dataSnapshot.getValue(Customer.class);
+                    getQremindApplication().setCustomerUser(custUser);
+                    nextActivityAfterLogin(CustomerHomePageActivity.class);
+                }
+                else if(role.equals(Constants.ROLE_VENDOR))
+                {
+                    Vendor vendorUser = dataSnapshot.getValue(Vendor.class);
+                    getQremindApplication().setVendorUser(vendorUser);
+                    nextActivityAfterLogin(ChooseTaskActivity.class);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Commons.handleCommonFirebaseError(firebaseError, getApplicationContext());
+            }
+        });
+    }
+
+    /**
+     * Move to next activity after login
+     * @param activityClass (some_activity).class
+     */
+    private void nextActivityAfterLogin(Class activityClass)
+    {
+        Intent intent = new Intent(LogoCoverPageActivity.this, activityClass);
+        startActivity(intent);
+        LogoCoverPageActivity.this.finish();
+    }
 }
