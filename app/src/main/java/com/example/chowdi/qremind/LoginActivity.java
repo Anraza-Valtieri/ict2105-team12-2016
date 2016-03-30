3be5b99132bee7c70e6bad2598cf835916279d65
Winnie Lew <lewmh.winnie@yahoo.com.sg>
2016-03-30 11:18:12 +0800

    dialog box to notify customer when queue has reached

--- app/src/main/java/com/example/chowdi/qremind/LoginActivity.java
+++ app/src/main/java/com/example/chowdi/qremind/LoginActivity.java
@@ -57,7 +57,6 @@ public class LoginActivity extends AppCompatActivity {
             prefs = getSharedPreferences(Constants.SHARE_PREF_LINK, MODE_PRIVATE);
             String role = prefs.getString(Constants.SHAREPREF_ROLE, null);
             if(role.equals(Constants.ROLE_CUSTOMER))
-                //nextActivityAfterLogin(CustomerGetQueueActivity.class);
                 nextActivityAfterLogin(CustomerHomePageActivity.class);
             else if(role.equals(Constants.ROLE_VENDOR))
                 nextActivityAfterLogin(ChooseTaskActivity.class);
@@ -291,7 +290,6 @@ public class LoginActivity extends AppCompatActivity {
                             public void onAuthenticated(AuthData authData) {
                                 saveAuthenticatedUserInfo(dataSnapshot.child("email").getValue().toString(), loginID, Constants.ROLE_CUSTOMER);
                                 Commons.dismissProgressDialog(pd);
-                                //nextActivityAfterLogin(CustomerGetQueueActivity.class);
                                 nextActivityAfterLogin(CustomerHomePageActivity.class);
                             }
 
