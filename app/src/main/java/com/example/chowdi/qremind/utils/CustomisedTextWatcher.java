8bfab4c1319e149965af2fba86b55c487a57b3c5
antonsalim <anton_salim@hotmail.com>
2016-04-02 11:03:18 +0800

    Class for every EditText TextChangeListener

---
+++ app/src/main/java/com/example/chowdi/qremind/utils/CustomisedTextWatcher.java
@@ -0,0 +1,63 @@
+package com.example.chowdi.qremind.utils;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.nio.charset.Charset;
+
+/**
+ * Created by anton on 2/4/2016.
+ */
+public class CustomisedTextWatcher implements TextWatcher, View.OnFocusChangeListener {
+
+    private TextView textView;
+    private EditText editText;
+    private int hintId;
+
+    public CustomisedTextWatcher(EditText editText, TextView textView, int hintId)
+    {
+        this.textView = textView;
+        this.editText = editText;
+        this.hintId = hintId;
+
+        editText.setOnFocusChangeListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        editText.setError(null);
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if(hasFocus) {
+            textView.setVisibility(View.VISIBLE);
+            if (hintId > 0)
+                editText.setHint("");
+        }
+        else
+        {
+            if(editText.getText().toString().isEmpty())
+            {
+                textView.setVisibility(View.INVISIBLE);
+                editText.setHint(hintId);
+            }
+            else
+            {
+                textView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+}
